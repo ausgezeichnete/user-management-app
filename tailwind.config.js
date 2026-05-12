d64127baf3999bcaf3a1799f841c0d577573cd68
Maha F. <ausgezeichnete@gmail.com>
2026-05-12 20:02:27 +0200

    commit all files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}", // scan all .ts and .tsx files inside src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
